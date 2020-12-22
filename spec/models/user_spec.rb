461ecd472c7e134fd88a9710d8a8ef8a2dbfdc99
Suguru <sugar961216@gmail.com>
2020-12-22 11:49:40 +0900

    change user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,8 +16,43 @@ RSpec.describe User, type: :model do
     user.valid?
     expect(user.errors[:first_name]).to include("can't be blank")
   end
-  it "is invalid without a last name"
-  it "is invalid without an email address"
-  it "is invalid with a duplicate email address"
-  it "returns a user's full name as a string"
+
+  it "is invalid without a last name" do
+    user = User.new(last_name: nil)
+    user.valid?
+    expect(user.errors[:last_name]).to include("can't be blank")
+  end
+
+  it "is invalid without an email address" do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it "is invalid with a duplicate email address" do
+    User.create(
+      first_name: "joe",
+      last_name: "tsster",
+      email: "test@example.com",
+      password: "password"
+    )
+    user = User.new(
+      first_name: "uu",
+      last_name: "test",
+      email: "test@example.com",
+      password: "password"
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
+
+  it "returns a user's full name as a string"  do
+    user = User.create(
+      first_name: "tarou",
+      last_name: "yamada",
+      email: "eafdf@dafsdf"
+    )
+    expect(user.name).to eq "tarou yamada"
+  end
+
 end
