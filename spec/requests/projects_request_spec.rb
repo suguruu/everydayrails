f78314add8e6790db3dccbbdfcc0844f9235b62e
Suguru <sugar961216@gmail.com>
2020-12-23 19:16:00 +0900

    add #create spec

--- spec/requests/projects_request_spec.rb
+++ spec/requests/projects_request_spec.rb
@@ -61,4 +61,19 @@ RSpec.describe ProjectsController, type: :controller do
       end
     end
   end
+  describe "#create" do
+    context "as an authenticated user" do
+      before do
+        @user = FactoryBot.create(:user)
+      end
+
+      it "adds a project" do
+        project_params = FactoryBot.attributes_for(:project)
+        sign_in @user
+        expect {
+          post :create, params: { project: project_params }
+        }.to change(@user.projects, :count).by(1)
+      end
+    end
+  end
 end
